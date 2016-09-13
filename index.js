ae94475e331a9dd4a283cc1b42b57e424d575957
Filipe Barroso <filipe.barroso@wingman.pt>
2016-09-13 15:19:53 +0100

    1309201615:19

--- index.js
+++ index.js
@@ -15,45 +15,29 @@ phantom.create()
         return sitepage.open('public/index.html');
     })
     .then(status => {
-        console.log(status);
         return sitepage.property('content');
     })
     .then(content => {
-        // console.log(content);
+        console.log("> start");
+        var frame = 0;
+        var target_fps = 30;
+        var frames = 0;
 
-        var defaultValue = 0;
-        setTimeout(function()
-        {
-          console.log("start");
-          var frame = 0;
-          var target_fps = 30;
-
-          printTotalDurationInSecond();
-
-          // var frames = sitepage.evaluate(function () {
-          //     return getTotalDurationInSecond();
-          // }) * target_fps;
-          var frames = target_fps * 1.5;// (TotalDurationInSeconds)
-
-          console.log("n:"+frames);
-
-          for(var frame = 0;frame<=frames;frame++)
-          {
-            sitepage.evaluate(function (time)
+        sitepage.evaluate(function () {
+            return getTotalDurationInSecond();
+        }).then(function(duration) {
+            frames = duration * target_fps;
+            for(var frame = 0;frame<=frames;frame++)
             {
-              pauseAnimationAt(time);
-            }, frame * (1 / target_fps));
-            sitepage.render('out/frame_'+pad(frame,padNumbers)+'.jpg', { format: 'jpg', quality: 90 });
-          }
-
-          printTotalDurationInSecond();
-
-          phInstance.exit();
-          sitepage.close();
-
-          createVideo();
-          console.log("> end");
-        }, 10);
+              sitepage.evaluate(function (time)
+              {
+                pauseAnimationAt(time);
+              }, frame * (1 / target_fps));
+              sitepage.render('out/frame_'+pad(frame,padNumbers)+'.jpg', { format: 'jpg', quality: 90 });
+            }
+        });
+        phInstance.exit();
+        sitepage.close();
     })
     .catch(error => {
         console.log(error);
@@ -62,7 +46,7 @@ phantom.create()
 
 
 function createVideo() {
-  var command = "ffmpeg -framerate 1/5 -i out/frame_%0"+padNumbers+"d.jpg -c:v libx264 -r 30 -pix_fmt yuv420p video/out.mp4";
+  var command = "ffmpeg -y -r 30 -i out/frame_%0"+padNumbers+"d.jpg -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4";
   exec(command, function callback(error, stdout, stderr) {
     if(error) {
       console.log("> video NOT created");
@@ -70,12 +54,12 @@ function createVideo() {
     else {
       console.log("> video created");
     }
-    deleteGeneratedImages();
+    console.log("> finished");
   });
 };
 
 function deleteGeneratedImages() {
-  var command = "rm out/frame_*";
+  var command = "rm -rf out";
   exec(command, function callback(error, stdout, stderr){
     if(error) {
       console.log("> images NOT deleted");
@@ -84,13 +68,6 @@ function deleteGeneratedImages() {
       console.log("> images deleted");
     }
   });
-};
-
-function printTotalDurationInSecond() {
-  var tmp = sitepage.evaluate(function () {
-      return getTotalDurationInSecond();
-  });
-  console.log(tmp);
 };
 
 function pad(a,b){return([1e15]+a).slice(-b)};
